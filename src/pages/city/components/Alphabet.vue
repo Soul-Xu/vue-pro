d33dd2b21fe8f2f99046936806603b451ac49889
Soul_xu <741930067@qq.com>
2020-03-12 23:46:28 +0800

    city list
    city list

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,32 @@
+<template>
+  <url class="list">
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+  </url>
+</template>
+
+<script>
+export default {
+  name: 'CityAlphabet'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .list
+    display: flex
+    flex-direction: column
+    justify-content: center  
+    list-style: none
+    position: absolute
+    top: 1.58rem
+    right: 0
+    bottom: 0
+    width: .4rem
+    .item
+      line-height: .4rem
+      text-align: center
+      color: $bgColor
+</style>
