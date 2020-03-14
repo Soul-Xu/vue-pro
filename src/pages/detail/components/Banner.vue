8a3eff678a0939a6717a6e1e1e2947d67abab789
Soul_xu <741930067@qq.com>
2020-03-14 19:47:58 +0800

    fade animation
    banner部分增加动画效果

--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -13,16 +13,19 @@
         </div>
       </div>
     </div>
-    <common-gallary
-      @close="handleGallayClose"
-      :imgs="bannerImgs"
-      v-show="showGallary"
-      ></common-gallary>
+    <FadeAnimation>
+      <common-gallary
+        @close="handleGallayClose"
+        :imgs="bannerImgs"
+        v-show="showGallary"
+        ></common-gallary>
+    </FadeAnimation>
   </div>
 </template>
 
 <script>
 import CommonGallary from 'common/gallary/Gallary'
+import FadeAnimation from 'common/fade/FadeAnimation'
 export default {
   name: 'DetailBanner',
   props: {
@@ -36,7 +39,8 @@ export default {
     }
   },
   components: {
-    CommonGallary
+    CommonGallary,
+    FadeAnimation
   },
   methods: {
     handleBannerClick () {
